ba4147883307890f2917c82dd9ec69a8f45c1a33
Tanvir-programmer <tanvir.webprogrammer@gmail.com>
2026-03-23 10:39:57 +0600

    modified code for authentication page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import { getServerSession } from "next-auth";
 import ContactPage from "./_components/Contact";
-import Contact from "./_components/Contact";
 import Hero from "./_components/Hero";
 import Packages from "./_components/Packages";
 import PriorityPage from "./_components/PriorityPage";
@@ -8,8 +8,10 @@ import Reviews from "./_components/Reviews";
 import TopDestinations from "./_components/TopDestinations";
 import Trustpoint from "./_components/Trustpoint";
 import UserCard from "./_components/UserCard";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home({}) {
+  const session = await getServerSession(authOptions);
   return (
     <div>
       <UserCard></UserCard>
@@ -20,6 +22,10 @@ export default function Home() {
       <Trustpoint></Trustpoint>
       <Reviews></Reviews>
       <ContactPage></ContactPage>
+      <div>
+        <p>User Server</p>
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
     </div>
   );
 }
